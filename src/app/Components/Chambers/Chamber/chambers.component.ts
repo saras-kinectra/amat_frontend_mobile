41f01790f0f371fbd23fca5781e5b24d45599cfa
saras-9490 <sarath@kinectra.com>
2019-01-29 18:43:30 +0530

    Implemented login for min_facetgroups_count Displaying got_code in chambers screen

--- src/app/Components/Chambers/Chamber/chambers.component.ts
+++ src/app/Components/Chambers/Chamber/chambers.component.ts
@@ -274,7 +274,7 @@ export class ChamberComponent implements OnInit {
       this.isButtonLabelCondition = false;
       this.formFiledUnderLine = false;
 
-      this.extraSelectedChamberCount = this.selectedChambersList.length-this.selectedPlatform.facets_count;
+      this.extraSelectedChamberCount = this.selectedChambersList.length - this.selectedPlatform.facets_count;
     } else {
       
       this.showErrorLabelCondition = true;
@@ -283,7 +283,7 @@ export class ChamberComponent implements OnInit {
       // this.isButtonLabelCondition = true;
       this.formFiledUnderLine = true;
 
-      if(this.selectedChambersList.length > 3) {
+      if(this.selectedChambersList.length >= this.selectedPlatform.min_facetgroups_count) {
 
         this.isButtonLabelCondition = true;
       } else {
